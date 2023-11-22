426c202a521cfd0d02d06d8f576083b9077a3bb3
Ades <adesintaaaa@gmail.com>
2023-11-22 23:56:22 +0800

    feat: add smooth scroll functionality in navbar

---
+++ src/components/landing-page/AboutUs.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import AboutImage from "../../assets/about-image.svg";
+
+const AboutUs = () => {
+  return (
+      <div id="aboutUs" className="p-10 flex flex-col justify-center items-center text-justify gap-y-4">
+        <h1 className="font-bold text-4xl pt-14">About Us</h1>
+        <p className="px-36 leading-loose">
+          Welcome to Fresh Market, your ultimate destination for a high-quality
+          shopping experience with fresh vegetables and fruits. We collaborate
+          with trusted local farmers to bring you fresh products directly from
+          the fields to your table. With a focus on diversity and deliciousness,
+          we offer a complete range of organic vegetables, exotic fruits, and
+          the finest selections of each season. At Fresh Market, we invite you
+          to explore the world of natural flavors, promote a healthy lifestyle,
+          and support sustainable farming. Thank you for choosing us as your
+          loyal partner on the journey towards true health and delight.
+        </p>
+        <img src={AboutImage} alt="" className="rounded-lg" />
+      </div>
+  );
+};
+
+export default AboutUs;
